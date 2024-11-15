01dd61b5b88ef41b4b03aa7fb768b06d04c098fd
Pushkar <pushkarjeemain@gmail.com>
2024-11-15 17:20:56 +0530

    Create PROJECT1.cpp

---
+++ PROJECT1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+  char colours_band[10]{"black","brown","red","orange","yellow","green","blue","violet","gray","white"};
+  char band1,band2,ban3;
+  cin>>band1>>band2>>ban3;
+  return 0;
+}
